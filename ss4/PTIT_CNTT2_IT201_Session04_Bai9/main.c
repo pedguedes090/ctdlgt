996d73d3bd7f91e8f4b084270153af45348162b5
pedguedes090 <noradavid319j23jaco@gmail.com>
2025-06-25 21:05:07 +0700

    upload

--- ss4/PTIT_CNTT2_IT201_Session04_Bai9/main.c
+++ ss4/PTIT_CNTT2_IT201_Session04_Bai9/main.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int searchNumber(int arr[],int left,int right,int num) {
+    int mid = left+(right-left)/2;
+    if (arr[mid]==num) {
+        return mid;
+    }
+    if (arr[mid]<num) {
+        return searchNumber(arr,left,mid-1,num);
+    }
+    if (arr[mid]>num) {
+        return searchNumber(arr,mid+1,right,num);
+    }
+}
 int main(void) {
-    printf("Hello, World!\n");
+    int n;
+    printf("vui long nhap so phan tu cua mang");
+    scanf("%d",&n);
+    int *arr=(int *)malloc(n*sizeof(int));
+    for(int i=0;i<n;i++) {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++) {
+        for(int j=i+1;j<n;j++) {
+            if(arr[i]>arr[j]) {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    int num;
+    printf("vui long nhap so can tim kiem ");
+    scanf("%d",&num);
+    int left =0;
+    int right =n-1;
+    if (searchNumber(arr,left,right,num)) {
+        printf("co phan tu trong mang");
+    }else {
+        printf("khong co phan tu trong mang");
+    }
+    free(arr);
     return 0;
 }
